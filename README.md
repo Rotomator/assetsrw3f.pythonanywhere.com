a9197f29943c8a3ef5b0892a8695ea75b7ec51c8
Ignacio911 <Ignacio911@gmail.com>
2018-05-30 07:14:52 +0000

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# https://github.com/Ignacio911/assetsrw3f.pythonanywhere.com/blob/master/sitemap.xml.gz?raw=true
+# https://assetsrw3f.pythonanywhere.com/sitemap.xml.gz
---
+++ pinglist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsrw3f.pythonanywhere.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsrw3f.pythonanywhere.com/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsrw3f.firebaseapp.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsrw3f.firebaseapp.com/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsrw3f.bitbucket.io/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsrw3f.bitbucket.io/sitemap.xml.gz
+wget -qO- https://www.google.com/webmasters/tools/ping?sitemap=https://assetsrw3f.netlify.com/sitemap.xml.gz
+wget -qO- https://www.bing.com/ping?siteMap=https://assetsrw3f.netlify.com/sitemap.xml.gz
